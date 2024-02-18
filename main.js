09caef255db4e9977ba97f70f0adeaedc0984d30
ristoxxx <lahteri@gmail.com>
2024-02-18 15:21:58 +0200

    code commented and cleaned

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-fetch('https://iot.fvh.fi/opendata/uiras/uiras2_v2.geojson')
-  .then(response => response.json())
-  .then(data => {
+fetch('https://iot.fvh.fi/opendata/uiras/uiras2_v2.geojson')                                                    // Fetch the JSON data
+  .then(response => response.json())                                                                            // Parse the JSON data   
+  .then(data => {                                                                                               // Process the JSON data
     // Find the value of properties/temp_water where properties/name is "uunisaari"
-    data.features.forEach(feature => {
-      if (feature.properties.name === "Uunisaari") {
-        const tempWater = Math.round(feature.properties.temp_water* 10) / 10 || feature.properties.temp_water;
-        console.log("Temperature of water at Uunisaari:", tempWater);
-        const temperatureContainer = document.getElementById('temperature-container');
-        const temperatureBox = document.createElement('div');
-        temperatureBox.classList.add('temperature-box');
-        temperatureBox.textContent = `Seawater temperature\n${tempWater} °C`;
-        temperatureContainer.appendChild(temperatureBox);
+    data.features.forEach(feature => {                                                                          // Loop through the features
+      if (feature.properties.name === "Uunisaari") {                                                            // Check if the name is "Uunisaari"
+        const tempWater = Math.round(feature.properties.temp_water* 10) / 10 || feature.properties.temp_water;  // Round to 1 decimal place
+        //console.log("Temperature of water at Uunisaari:", tempWater);
+        const temperatureContainer = document.getElementById('temperature-container');                         // Get the temperature container element 
+        const temperatureBox = document.createElement('div');                                                  // Create the temperature box element
+        temperatureBox.classList.add('temperature-box');                                                       // Add the class "temperature-box"
+        temperatureBox.textContent = `Seawater temperature\n${tempWater} °C`;                                  // Set the text content of the temperature box
+        temperatureContainer.appendChild(temperatureBox);                                                      // Append the temperature box to the temperature container
       }
     });
   })
-  .catch(error => {
+  .catch(error => {                                                                                             // Catch any errors
     console.error('Error fetching the JSON data:', error);
   });
